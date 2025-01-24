044aed51c46d58c2f80fd1764668888cf9a3fa4f
mitsuiakane <akane.sumitomo@gmail.com>
2025-01-24 23:58:59 +0900

    18

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Link from "next/link";
+import Image from "next/image";
 
 const ArticleList = () => {
   return (
-    <div>ArticleList</div>
+    <div>
+        <article>
+         <Link href="#">
+         <Image src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="" width={1280} height={300}/>
+         </Link>   
+        </article>
+    </div>
+    
   )
 }
 
