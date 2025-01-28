f834c0e70ee7ab9b1334c8ce31391b6d963e9d20
mitsuiakane <akane.sumitomo@gmail.com>
2025-01-28 21:59:59 +0900

    2分30秒

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,39 @@
+
+// import React from 'react'
+// import Image from 'next/image'
+// import {getDetailArticle} from "@/blogAPI"
+
+// const Article = async ({params}: {params:{ id: string }})=> {
+//   const detailArticle =await getDetailArticle(params.id)
+//   console.log(detailArticle)
+//   return (
+//      <div className="max-w-3xl mx-auto p-5">
+//         <Image src="https://picsum.photos/1280/300" alt="" width={1280} height={300}/>
+//         <h1 className="text-4xl text-center mb-10 mt-10">{detailArticle.title} </h1>
+//         <div>
+//          <p className="text-lg text-center leading-relaxed text-justify">
+//          {detailArticle.content} 
+//          </p>
+//         </div>
+//      </div>
+
+//   )
+// }
+
+// export default Article
+
+import React from 'react'
+
+const Article=({params}: {params:{ id: string }})=> {
+    console.log(params.id)
+  return (
+    <div>page</div>
+  )
+}
+
+export default Article
+
+
+
+
+
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,4 +1,5 @@
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 export const getAllArticles =async():Promise<Article[]>=>{
     const res =await fetch(`http://localhost:3001/posts`,{cache:"no-store"})
 
@@ -8,5 +9,24 @@ export const getAllArticles =async():Promise<Article[]>=>{
     await new Promise ((resolve)=>setTimeout(resolve,1500));
     const articles =await res.json();
     return articles;
+
+};
+
+
+export const getDetailArticles =async(id:string):Promise<Article>=>{
+   
+    const res =await fetch(`http://localhost:3001/posts/${id}`,{next:{revalidate:60},})
+    
+    if (res.status===400){
+        notFound();
+    }
+
+
+    if (!res.ok){
+    throw new Error("エラーが発生しました");
+    }
+    await new Promise ((resolve)=>setTimeout(resolve,1500));
+    const article =await res.json();
+    return article;
 
 };
