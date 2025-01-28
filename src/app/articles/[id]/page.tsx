5eccf07840f317e16184b674dcfcc76d0d46f5de
mitsuiakane <akane.sumitomo@gmail.com>
2025-01-28 23:09:19 +0900

    28

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,6 +1,6 @@
 
 // import React from 'react'
-// import Image from 'next/image'
+import Image from 'next/image'
 // import {getDetailArticle} from "@/blogAPI"
 
 // const Article = async ({params}: {params:{ id: string }})=> {
@@ -27,7 +27,14 @@ import React from 'react'
 const Article=({params}: {params:{ id: string }})=> {
     console.log(params.id)
   return (
-    <div>page</div>
+    <div className="max-w-3xl mx-auto p-5"><Image src="https://picsum.photos/1280/300" alt="" width={1280} height={300}/>
+    <h1 className="text-4xl text-center mb-10 mt-10">
+      ここがタイトリです
+    </h1>
+    <div>
+      <p className="text-lg text-center leading-relaxed text-justify">ここが本文です</p>  
+    </div>
+    </div>
   )
 }
 
